a32a9da32f4e494289656fa809af92aed30779cd
user.yash <yashmurugan18@gmail.com>
2026-03-06 15:15:29 +0530

    uc10

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,19 +1,23 @@
 import java.util.*;
 import java.util.Scanner;
 public class PalindromeCheckerApp {
-        static boolean isPalindrome(String str, int start, int end) {
-            if (start >= end)
-                return true;
-            if (str.charAt(start) != str.charAt(end))
-                return false;
-            return isPalindrome(str, start + 1, end - 1);
-        }
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             System.out.print("Enter a string: ");
             String input = sc.nextLine();
-            boolean result = isPalindrome(input, 0, input.length() - 1);
-            if (result)
+            String normalized = input.replaceAll("\\s+", "").toLowerCase();
+            int start = 0;
+            int end = normalized.length() - 1;
+            boolean isPalindrome = true;
+            while (start < end) {
+                if (normalized.charAt(start) != normalized.charAt(end)) {
+                    isPalindrome = false;
+                    break;
+                }
+                start++;
+                end--;
+            }
+            if (isPalindrome)
                 System.out.println("The string is a Palindrome.");
             else
                 System.out.println("The string is NOT a Palindrome.");
